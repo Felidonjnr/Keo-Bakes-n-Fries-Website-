80cfd7481d16f98d44d295e37d279154912cace3
Felidonjnr <godshandudoh@gmail.com>
2026-03-23 13:54:01 +0100

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -69,7 +69,7 @@ export const MENU_CATEGORIES = [
     items: [
       { name: "Classic Samosas", description: "Crispy triangular pastries, seasoned meat & vegetables.", image: "https://res.cloudinary.com/drcroxtgs/image/upload/v1774270243/image-42_bv6i2n.jpg" },
       { name: "Puff Puff (Regular)", description: "Warm, golden-fried dough balls. Timeless staple.", image: "https://res.cloudinary.com/drcroxtgs/image/upload/v1773864785/IMG-20260313-WA0061_ppyskx.jpg" },
-      { name: "Spring Rolls", description: "Delicate crispy rolls, savoury fillings, perfectly seasoned.", image: "input_file_2.png" },
+      { name: "Spring Rolls", description: "Delicate crispy rolls, savoury fillings, perfectly seasoned.", image: "https://res.cloudinary.com/drcroxtgs/image/upload/v1774269596/image-45_mmwxkw.jpg" },
     ]
   },
   {
